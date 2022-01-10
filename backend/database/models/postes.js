7cab2e5715871bd17f2e35f025413cb253759d3c
OmarHamdann <omar.haamdan@gmail.com>
2022-01-10 23:28:46 +0200

    create media in post and useing multer pakage

--- backend/database/models/postes.js
+++ backend/database/models/postes.js
@@ -4,6 +4,8 @@ const postes = new mongoose.Schema({
   userName: { type: mongoose.Schema.Types.String, ref: "User" },
 
   poste: { type: String, required: true },
+  fileName: { type: String },
+  mimetype: { type: String },
   like: { type: Number, default: 0 },
   date: { type: Date, default: Date.now() },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
--- backend/routes/postes.js
+++ backend/routes/postes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const { createNewPostes } = require("../controllers/postes");
 
@@ -7,5 +9,5 @@ const authentication = require("../middleware/authentication");
 
 const postesRouter = express.Router();
 
-postesRouter.post("/", authentication, createNewPostes);
+postesRouter.post("/", authentication, upload.single("media"), createNewPostes);
 module.exports = postesRouter;
