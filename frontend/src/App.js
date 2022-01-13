0b933ee86b046acc65dbce986d434eb08d7065bf
OmarHamdann <omar.haamdan@gmail.com>
2022-01-13 11:41:56 +0200

    invoke login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import Register from "./components/Register";
+import Login from "./components/Login";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
+  const token = localStorage.getItem("userToken");
+  const [isLoggedIn, setisLoggedIn] = useState(false);
   return (
     <div className="App">
       <h1>Welcome To App</h1>
+
       <Routes>
         {" "}
         <Route path="/" element={<Register />} />
-        <Route path="/users" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={<Login loggedin={setisLoggedIn} />}
+          setisLoggedIn={setisLoggedIn}
+        />
         <Route
           path="*"
           element={
