43cbedb720e2dbccf70a83b1756f42eb40c069a3
OmarHamdann <omar.haamdan@gmail.com>
2022-01-13 11:05:45 +0200

    invoke register page in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
-
-function App() {
+import Register from "./components/Register";
+import { Routes, Route } from "react-router-dom";
+const App = () => {
   return (
     <div className="App">
       <h1>Welcome To App</h1>
+      <Routes>
+        {" "}
+        <Route path="/" element={<Register />} />
+        <Route path="/users" element={<Register />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <h1 className="not-found">404</h1>
+              <p>This page not found</p>
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
