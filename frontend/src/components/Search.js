ee8a3b6008e2c96462920a9365b114aadc4a5c3c
OmarHamdann <omar.haamdan@gmail.com>
2022-01-15 23:49:49 +0200

    create Search component to search posts from userName

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
+const Search = ({ setPost, allPost }) => {
+  const [search, setSearch] = useState("");
+  const [cheack, setCheack] = useState("");
+
+  return (
+    <div className="blue form">
+      <input
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        onKeyPress={(event) => {
+          if (event.key == "Enter") {
+            if (!search) {
+              setCheack("");
+              allPost();
+            } else {
+              axios
+                //send data from body object
+                .get(`http://localhost:5000/postes/search_1?userName=${search}`)
+                .then((result) => {
+                  setPost(result.data.postes);
+                  setCheack("done");
+                })
+                .catch((err) => {
+                  //if error
+                  setCheack("error");
+                  setPost("");
+                  console.log(err);
+                });
+            }
+          }
+        }}
+        type="search"
+        placeholder="userName..."
+      />
+      {
+        //if error or undfinde
+        cheack == "error" ? (
+          <div className="error">no such user!!</div>
+        ) : (
+          //if undfinde => no change
+          <div />
+        )
+      }
+    </div>
+  );
+};
+
+export default Search;
