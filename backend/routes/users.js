5ddaac964ab6f36f1ae3f818208b1d93c18843fc
OmarHamdann <omar.haamdan@gmail.com>
2022-01-16 12:57:50 +0200

    import multer in users router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { createNewUser } = require("../controllers/users");
-
+const multer = require("multer");
+const path = require("path");
 // define router
 const usersRouter = express.Router();
+//upload avatar
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  },
+});
+const upload = multer({ storage: storage });
 
-usersRouter.post("/", createNewUser);
+usersRouter.post("/", upload.single("media"), createNewUser);
 
 module.exports = usersRouter;
