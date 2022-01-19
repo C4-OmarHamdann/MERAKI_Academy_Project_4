cd9c9d0acd63dd5ae1cc14648521eeaa5b74ee9a
OmarHamdann <omar.haamdan@gmail.com>
2022-01-20 00:05:17 +0200

    add asideright componet

---
+++ frontend/src/components/AsideRight.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Search from "./Search";
+
+const AsideRight = ({ setPostes, allPosts }) => {
+  return (
+    <>
+      <div className="aside-right-items">
+        <Search setPost={setPostes} allPost={allPosts} />
+        <h2>What's happening</h2>
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+        <hr />
+        <div className="aside-right-item">
+          <p>Cricket . LIVE</p>
+          <h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, unde?
+          </h4>
+          <p>
+            Trending with <span>#engvsindia</span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AsideRight;
