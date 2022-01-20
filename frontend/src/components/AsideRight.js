a33df3637c45f545c67fd7766eff747838bf34da
OmarHamdann <omar.haamdan@gmail.com>
2022-01-20 16:13:26 +0200

    api news

--- frontend/src/components/AsideRight.js
+++ frontend/src/components/AsideRight.js
@@ -1,88 +1,32 @@
 import React from "react";
 import Search from "./Search";
 
-const AsideRight = ({ setPostes, allPosts }) => {
+import { useState } from "react";
+
+const AsideRight = ({ setPostes, allPosts, news }) => {
+  const [limit, setLimit] = useState(6);
+
+  const newsMap = news.slice(0, limit).map((el, i) => {
+    return (
+      <div key={i} className="aside-right-item">
+        <p>Cricket . LIVE</p>
+        <h4>{el.title}</h4>
+        <p>
+          Trending with <span>#{el.author}</span>
+        </p>
+        <hr />
+      </div>
+    );
+  });
   return (
     <>
       <div className="aside-right-items">
         <Search setPost={setPostes} allPost={allPosts} />
-        <h2>What's happening</h2>
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
-        <hr />
-        <div className="aside-right-item">
-          <p>Cricket . LIVE</p>
-          <h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, unde?
-          </h4>
-          <p>
-            Trending with <span>#engvsindia</span>
-          </p>
-        </div>
+        <h2>What's happening ?</h2>
+        {newsMap}
+        <a className="showMore" onClick={() => setLimit(limit + 4)}>
+          Show more
+        </a>
       </div>
     </>
   );
